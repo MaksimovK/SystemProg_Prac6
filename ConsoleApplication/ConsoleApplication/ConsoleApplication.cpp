4cbbd19c91a4301bdee232a0b505c843344678a3
Jozer <kmakismov@yandex.ru>
2024-02-10 09:55:46 +0500

    first commit

---
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <list>
+
+using namespace std;
+
+int main() {
+    system("chcp 1251");
+    setlocale(LC_ALL, "ru");
+    string str;
+    cout << "Введите строку: ";
+    getline(cin, str);
+
+    stringstream ss(str);
+    string word;
+    list<string> words;
+
+    while (ss >> word) {
+        words.push_back(word);
+    }
+
+    words.sort();
+
+    for (const auto& w : words) {
+        cout << w << ' ';
+    }
+
+    return 0;
+}
